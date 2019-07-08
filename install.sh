ae08cbd7b498ebcb9a25e1e9846f8d356474af84
azt501 <46714940+azt501@users.noreply.github.com>
2019-07-08 14:36:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo apt-get update --fix-missing
 sudo apt-get upgrade
 sudo apt-get install tmux vim git
 sudo apt-get install i2c-tools build-essential
+sudo apt-get install mosquitto mosquitto-clients
 # python2 libraries
 sudo apt-get install python-dev python-pyaudio python-numpy python-scipy python-smbus python-pip python-blinkt libc-ares-dev libwebsockets-dev libssl-dev xsltproc docbook-xsl python-blinkt
 sudo pip install paho-mqtt
