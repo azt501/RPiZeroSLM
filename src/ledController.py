d0f79d0f6ca24efc37146db4c67b65b05a04c722
azt501 <46714940+azt501@users.noreply.github.com>
2019-07-09 10:14:05 +0100

    Update ledController.py

--- src/ledController.py
+++ src/ledController.py
@@ -13,9 +13,9 @@ def on_message(client, userdata, message):
     print(currentdB)
     readyToGo = True
 
-R = [0,0,51,105,180,255,255,255]
-G = [255,255,180,100,0,0,0,0]
-B = [0,0,51,105,180,255,150,0]
+R = [0,0,65,105,180,255,255,255]
+G = [255,230,150,100,50,0,0,0]
+B = [0,0,0,0,0,0,0,0]
 client = mqtt.Client("P1")
 client.connect("172.16.21.105")
 client.subscribe("sensor1")
@@ -26,15 +26,21 @@ while not readyToGo:
 	on_message
 
 while readyToGo:
+	for i in range (0,8):
+		set_pixel(i,0,0,0)
 	if currentdB != olddB:
 		currentPercent = currentdB / 1.10
 		LEDsToLight = int(math.floor(currentPercent / 12.5))	#checks the number of LEDs to light all the way up
 		percentToLight = currentPercent / 12.5 - math.floor(currentPercent / 12.5)	#checks the amount to light the next LED
+		if LEDsToLight > 7: 
+			LEDsToLight = 7
+			percentToLight = 1
 
 		for i in range (0,LEDsToLight):
 			set_pixel(i, R[i],G[i],B[i])
 			print(R[i], G[i], B[i])
 
-		set_pixel(LEDsToLight + 1, int(R[LEDsToLight+1]*percentToLight), int(G[LEDsToLight+1]*percentToLight), int(B[LEDsToLight+1]*percentToLight))
-		print("final rgb: ", int(R[LEDsToLight+1]*percentToLight), int(G[LEDsToLight+1]*percentToLight), int(B[LEDsToLight+1]*percentToLight))
+		set_pixel(LEDsToLight, int(R[LEDsToLight]*percentToLight), int(G[LEDsToLight]*percentToLight), int(B[LEDsToLight]*percentToLight))
+		print("final rgb: ", int(R[LEDsToLight]*percentToLight), int(G[LEDsToLight]*percentToLight), int(B[LEDsToLight]*percentToLight))
 		olddB = currentdB
+		show()
